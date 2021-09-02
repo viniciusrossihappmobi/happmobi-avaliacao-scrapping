2c473e11e9b59663c9f0fe5e722dcc71817a84d1
Vinicius Rossi <vinicius.rossi@happmobi.com.br>
2021-09-02 10:25:15 -0300

    Tratamento para erros

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 (async () => {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://interno.happmobi/Happmobi_HAPP/HAPP000_Repositorio/converter-avaliacao/');
     await page.type('.input-text', process.argv[2]);
     await page.click('.btn');
     await page.waitForSelector('#out');
-    await page.waitForFunction('document.querySelector(\'#out\').value.includes(\'<avaliacao>\')');
+    await page.waitForFunction('document.querySelector(\'#out\').value.length > 0');
     const data = await page.evaluate(() => {
         // eslint-disable-next-line no-undef
         return document.querySelector('#out').value;
     });
 
+    if (data.includes('Não foram encontrados dados!')) {
+        console.log('URL Inválida!');
+        await browser.close();
+        return;
+    }
+
     fs.writeFile('avaliacao.xml', data, (error) => {
-        if (error) console.log(error);
+        if (error) return console.log(error);
     });
 
     await browser.close();
